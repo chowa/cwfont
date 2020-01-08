c679bffda8896e23c8e91ace98b5251874629b56
smoking rabbit <xuecong8226@gmail.com>
2020-01-08 17:25:19 +0800

    fix export

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as _utils from './utils';
 import * as _log from './log';
 import { defaultOptions } from './options';
 
-export { default as ChowaGenerator } from './generator';
+export { default as Generator } from './generator';
 export const utils = _utils;
 export const log = _log;
 export const config = defaultOptions;
